4150fd6ddbd080dd2bd4e6f3b93062b49e7d7b68
amilpa <amilpa2020@gmail.com>
2023-02-10 19:27:20 +0530

    added .vscode filetype ignore

--- lua/pluginconfig/tele.lua
+++ lua/pluginconfig/tele.lua
@@ -1,3 +1,3 @@
 require('telescope').setup{
-	defaults = { file_ignore_patterns = {"packer_compiled.lua",".git",".gitignore",".exe" }} 
+	defaults = { file_ignore_patterns = {"packer_compiled.lua",".git",".gitignore",".exe" ,".vscode" }} 
 }
