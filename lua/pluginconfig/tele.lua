497c6ae0d64c16e5934cc9fab19f23fc66880a05
amilpa <amilpa2020@gmail.com>
2023-02-10 22:40:59 +0530

    commit

--- lua/pluginconfig/tele.lua
+++ lua/pluginconfig/tele.lua
@@ -1,3 +1,3 @@
 require('telescope').setup{
-	defaults = { file_ignore_patterns = {"packer_compiled.lua",".git",".gitignore",".exe" ,".vscode" }} 
+	defaults = { file_ignore_patterns = {"packer_compiled.lua",".git",".gitignore",".exe" ,".vscode" ,"node_modules"}} 
 }
