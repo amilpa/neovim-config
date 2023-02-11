98ba228c1bef6693f2cc048736bb1ea1e7d4f8ff
amilpa <amilpa2020@gmail.com>
2023-02-11 22:13:55 +0530

    added custom snippets

---
+++ lua/pluginconfig/snip.lua
@@ -0,0 +1,3 @@
+
+require("luasnip.loaders.from_vscode").lazy_load { paths = { "~/AppData/Local/nvim/lua/pluginconfig/snippets/python" } }
+require("luasnip.loaders.from_vscode").lazy_load { paths = { "~/AppData/Local/nvim/lua/pluginconfig/snippets/javascript" } }
