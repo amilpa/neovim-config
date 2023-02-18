435383ccc30ebe42a484d932b3b4a4f5b94fa808
Amil P A <amilpa2020@gmail.com>
2023-02-18 19:28:25 +0530

    changed up telescope

--- lua/pluginconfig/tele.lua
+++ lua/pluginconfig/tele.lua
@@ -1,3 +1,13 @@
+
+local actions = require("telescope.actions")
+
 require('telescope').setup{
-	defaults = { file_ignore_patterns = {"packer_compiled.lua",".git",".gitignore",".exe" ,".vscode" ,"node_modules",".jpg",".png",".out"}} 
+	defaults = { file_ignore_patterns = {"packer_compiled.lua",".git",".gitignore",".exe" ,".vscode" ,"node_modules",".jpg",".png",".out"},
+
+	mappings = {
+		n = {
+			["<C-s>"] =  actions.select_vertical
+		}
+	}
+},
 }
