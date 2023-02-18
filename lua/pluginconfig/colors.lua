53d66151f251b0d2cb9bfcff2e6d4027e9a09c5c
Amil P A <amilpa2020@gmail.com>
2023-02-18 19:04:07 +0530

    changed up colourschemes

--- lua/pluginconfig/colors.lua
+++ lua/pluginconfig/colors.lua
@@ -2,8 +2,40 @@
 
 require("tokyonight").setup({
 	transparent = true,
+	on_highlights = function(hl, c)
+    local prompt = "#2d3149"
+    hl.TelescopeNormal = {
+      bg = "None",
+      fg = c.fg_dark,
+    }
+    -- hl.TelescopeBorder = {
+    --   bg = c.bg_dark,
+    --   fg = c.bg_dark,
+    -- }
+    -- hl.TelescopePromptNormal = {
+    --   bg = "None",
+    -- }
+    -- hl.TelescopePromptBorder = {
+    --   bg = "None",
+    --   fg = prompt,
+    -- }
+    -- hl.TelescopePromptTitle = {
+    --   bg = "None",
+    --   fg = prompt,
+    -- }
+    -- hl.TelescopePreviewTitle = {
+    --   bg = c.bg_dark,
+    --   fg = c.bg_dark,
+    -- }
+    -- hl.TelescopeResultsTitle = {
+    --   bg = "None",
+    --   fg = c.bg_dark,
+    -- }
+  end,
 })
 
 vim.cmd('colorscheme tokyonight')
+
 vim.cmd('hi LineNr guibg=none guifg=#ffffff')
 vim.cmd('hi comment guifg=#449c70')
+
