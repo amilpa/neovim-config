920e468353f56682d70770c85f5b131701681ce7
Amil P A <amilpa2020@gmail.com>
2023-02-19 17:44:00 +0530

    made changes to options

--- lua/amilpa/init.lua
+++ lua/amilpa/init.lua
@@ -1,9 +1,11 @@
 require("amilpa.remap")
 require("amilpa.coderunner")
 
+vim.opt.nu = true
 vim.opt.relativenumber= true
 vim.opt.mouse = 'a'
 vim.opt.hlsearch = false
+vim.opt.incsearch = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.cursorline = false
