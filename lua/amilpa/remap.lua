2e0669582615c98ea1778d2796ec1133abd7dd45
Amil P A <amilpa2020@gmail.com>
2023-02-19 18:44:49 +0530

    turned off jk to escape

--- lua/amilpa/remap.lua
+++ lua/amilpa/remap.lua
@@ -1,12 +1,13 @@
 vim.g.mapleader =" "
 
+
 vim.keymap.set('n' , '<leader>w' , '<cmd>write<cr>' , {desc = 'Save'})
 vim.keymap.set('n' , '<S-l>' , '<cmd>bnext<cr>' , {desc = 'Next buffer'})
 vim.keymap.set('n' , '<S-h>' , '<cmd>bprevious<cr>' , {desc = 'Previous buffer'})
 vim.keymap.set('n' , '<leader>pv' , '<cmd>Ex<cr>' , {desc = 'Netrw'})
 vim.keymap.set('n' , '<leader>q' , ':q' , {desc = 'Quit'})
 vim.keymap.set('n' , '<leader>e' , ':q!' , {desc = 'Quit without saving'})
-vim.keymap.set('i', 'jk', '<Esc>')
+--vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('n', '<leader>t', '<cmd>Telescope find_files hidden=true<cr>')
 vim.keymap.set('n', '<leader>g', '<cmd>Telescope live_grep hidden=true<cr>')
 vim.keymap.set('n', '<leader>b', '<cmd>Telescope buffers hidden=true<cr>')
@@ -31,8 +32,6 @@ vim.keymap.set('n', '<leader>pd', '<cmd>cd %:h<cr>')
 vim.keymap.set('n', '<leader>sl', '<cmd>SessionManager load_session<cr>')
 vim.keymap.set('n', '<leader>ss', '<cmd>SessionManager save_current_session<cr>')
 vim.keymap.set('n', '<leader>sd', '<cmd>SessionManager delete_session<cr>')
-
-
 
 
 
