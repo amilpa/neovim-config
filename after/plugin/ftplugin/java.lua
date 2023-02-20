4c418123f9a7449616cdbca288e81ef3f167ed73
Amil P A <amilpa2020@gmail.com>
2023-02-20 17:32:12 +0530

    tried java config

--- after/plugin/ftplugin/java.lua
+++ after/plugin/ftplugin/java.lua
@@ -1,7 +1,7 @@
-local jdtls_config = require("javaconfig.jdtls").setup()
-local pkg_status, jdtls = pcall(require,"jdtls")
-if not pkg_status then
-  vim.notify("unable to load nvim-jdtls", "error")
-  return
-end
-jdtls.start_or_attach(jdtls_config)
+-- local jdtls_config = require("javaconfig.jdtls").setup()
+-- local pkg_status, jdtls = pcall(require,"jdtls")
+-- if not pkg_status then
+--   vim.notify("unable to load nvim-jdtls", "error")
+--   return
+-- end
+-- jdtls.start_or_attach(jdtls_config)
