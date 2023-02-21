a2834bce186cf389bc248e73bf5ceda5eb597808
Amil P A <amilpa2020@gmail.com>
2023-02-21 11:18:14 +0530

    added support for coderun java

--- lua/amilpa/coderunner.lua
+++ lua/amilpa/coderunner.lua
@@ -3,3 +3,4 @@ local api = vim.api
 api.nvim_create_autocmd("FileType", { pattern = "python", command = [[nnoremap <buffer><silent> <A-g> :TermExec cmd="python3 %:t" dir=%<CR>]] })
 api.nvim_create_autocmd("FileType", { pattern = {"html","css"}, command = [[nnoremap <buffer><silent> <A-g> :TermExec cmd="live-server" dir=%<CR>]] })
 api.nvim_create_autocmd("FileType", { pattern = "c", command = [[nnoremap <buffer><silent> <A-g> :TermExec cmd="gcc % && ./a.out" dir=%<CR>]] })
+api.nvim_create_autocmd("FileType", { pattern = "java", command = [[nnoremap <buffer><silent> <A-g> :TermExec cmd="javac %:t && java %:t:r.class" dir=%<CR>]] })
