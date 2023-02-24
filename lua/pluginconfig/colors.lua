68ff8f55ec19741e7e00f8a12d1d4397fcd2050e
Amil P A <amilpa2020@gmail.com>
2023-02-24 16:41:51 +0530

    switched back to transparency

--- lua/pluginconfig/colors.lua
+++ lua/pluginconfig/colors.lua
@@ -1,57 +1,57 @@
 
 
--- require("tokyonight").setup({
--- 	transparent = true,
--- 	on_highlights = function(hl, c)
---     local prompt = "#2d3149"
---     hl.TelescopeNormal = {
---       bg = "None",
---       fg = c.fg_dark,
---     }
--- 	-- hl.TelescopePromptNormal = {
--- 	--         bg = 'None',
--- 	-- 	fg = c.fg_dark,
--- 	--     }
--- 	--     hl.TelescopePromptBorder = {
--- 	--         bg = '#2d3149'
--- 	--     }
--- 	--     hl.TelescopePromptTitle = {
--- 	--         fg = '#2d3149',
--- 	--         bg = 'None'
--- 	--     }
--- 	--     hl.TelescopePreviewTitle = {
--- 	--         fg = '#1F2335',
--- 	--         bg = '#1F2335'
--- 	--     }
--- 	--     hl.TelescopeResultsTitle = {
--- 	--         fg = '#1F2335',
--- 	--         bg = '#1F2335'
--- 	--     }
---     -- hl.TelescopeBorder = {
---     --   bg = c.bg_dark,
---     --   fg = c.bg_dark,
---     -- }
---     -- hl.TelescopePromptNormal = {
---     --   bg = "None",
---     -- }
---     -- hl.TelescopePromptBorder = {
---     --   bg = "None",
---     --   fg = prompt,
---     -- }
---     -- hl.TelescopePromptTitle = {
---     --   bg = "None",
---     --   fg = prompt,
---     -- }
---     -- hl.TelescopePreviewTitle = {
---     --   bg = c.bg_dark,
---     --   fg = c.bg_dark,
---     -- }
---     -- hl.TelescopeResultsTitle = {
---     --   bg = "None",
---     --   fg = c.bg_dark,
---     -- }
---   end,
--- })
+require("tokyonight").setup({
+	transparent = true,
+	on_highlights = function(hl, c)
+    local prompt = "#2d3149"
+    hl.TelescopeNormal = {
+      bg = "None",
+      fg = c.fg_dark,
+    }
+	-- hl.TelescopePromptNormal = {
+	--         bg = 'None',
+	-- 	fg = c.fg_dark,
+	--     }
+	--     hl.TelescopePromptBorder = {
+	--         bg = '#2d3149'
+	--     }
+	--     hl.TelescopePromptTitle = {
+	--         fg = '#2d3149',
+	--         bg = 'None'
+	--     }
+	--     hl.TelescopePreviewTitle = {
+	--         fg = '#1F2335',
+	--         bg = '#1F2335'
+	--     }
+	--     hl.TelescopeResultsTitle = {
+	--         fg = '#1F2335',
+	--         bg = '#1F2335'
+	--     }
+    -- hl.TelescopeBorder = {
+    --   bg = c.bg_dark,
+    --   fg = c.bg_dark,
+    -- }
+    -- hl.TelescopePromptNormal = {
+    --   bg = "None",
+    -- }
+    -- hl.TelescopePromptBorder = {
+    --   bg = "None",
+    --   fg = prompt,
+    -- }
+    -- hl.TelescopePromptTitle = {
+    --   bg = "None",
+    --   fg = prompt,
+    -- }
+    -- hl.TelescopePreviewTitle = {
+    --   bg = c.bg_dark,
+    --   fg = c.bg_dark,
+    -- }
+    -- hl.TelescopeResultsTitle = {
+    --   bg = "None",
+    --   fg = c.bg_dark,
+    -- }
+  end,
+})
 
 vim.cmd('colorscheme tokyonight')
 
