42c7636a5a3902fad2f7a5fb187294d47dcef620
Amil P A <amilpa2020@gmail.com>
2023-02-25 12:04:35 +0530

    added null div snippet and tsx parser

--- lua/pluginconfig/syntax.lua
+++ lua/pluginconfig/syntax.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "html","css","c","python","cpp", "lua", "vim", "help" ,"javascript" , "typescript"},
+  ensure_installed = { "html","css","c","python","cpp", "lua", "vim", "help" ,"javascript" , "typescript" , "tsx"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
