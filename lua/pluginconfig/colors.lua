72ce5a4ee107a4d2c690b5582eda3436f09497f9
Amil P A <amilpa2020@gmail.com>
2023-02-26 20:32:30 +0530

    fixed neosolarized theme

--- lua/pluginconfig/colors.lua
+++ lua/pluginconfig/colors.lua
@@ -79,7 +79,7 @@ Color.new('black', '#000000')
 Group.new('Normal', colors.base1, colors.NONE, styles.NONE)
 Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
 Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
-Group.new('Visual', colors.none, colors.base03, styles.reverse)
+-- Group.new('Visual', colors.none, colors.base03, styles.reverse)
 
 local cError = groups.Error.fg
 local cInfo = groups.Information.fg
