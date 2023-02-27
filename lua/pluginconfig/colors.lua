1d77aa4073e9edaf5d238b4c896e9009908ec2ec
Amil P A <amilpa2020@gmail.com>
2023-02-27 18:49:32 +0530

    turned off search highlight

--- lua/pluginconfig/colors.lua
+++ lua/pluginconfig/colors.lua
@@ -79,6 +79,7 @@ Color.new('black', '#000000')
 Group.new('Normal', colors.base1, colors.NONE, styles.NONE)
 Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
 Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
+Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
 -- Group.new('Visual', colors.none, colors.base03, styles.reverse)
 
 local cError = groups.Error.fg
@@ -103,3 +104,5 @@ Group.new("HoverBorder", colors.yellow, colors.none, styles.NONE)
 -- 		theme = 'ayu_dark'
 -- 	}
 -- }
+--
+vim.opt.hlsearch = false
