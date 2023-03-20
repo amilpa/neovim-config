36085831b0a04529da570b828fda6598d92f9547
Amil P A <amilpa2020@gmail.com>
2023-03-20 18:14:47 +0530

    added react

--- lua/amilpa/coderunner.lua
+++ lua/amilpa/coderunner.lua
@@ -4,3 +4,4 @@ api.nvim_create_autocmd("FileType", { pattern = "python", command = [[nnoremap <
 api.nvim_create_autocmd("FileType", { pattern = {"html","css"}, command = [[nnoremap <buffer><silent> <A-g> :TermExec cmd="live-server" dir=%<CR>]] })
 api.nvim_create_autocmd("FileType", { pattern = "c", command = [[nnoremap <buffer><silent> <A-g> :TermExec cmd="gcc % && ./a.out" dir=%<CR>]] })
 api.nvim_create_autocmd("FileType", { pattern = "java", command = [[nnoremap <buffer><silent> <A-g> :TermExec cmd="javac %:t && java %:t:r" dir=%<CR>]] })
+api.nvim_create_autocmd("FileType", { pattern = "javascriptreact", command = [[nnoremap <buffer><silent> <A-g> :TermExec cmd="npm run dev" dir=%<CR>]] })
