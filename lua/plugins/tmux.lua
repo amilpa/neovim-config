0e2f5bc97a8be78be5818e10b25004bc48476adf
amilpa <amilpa2020@gmail.com>
2023-05-07 10:52:55 +0530

    added tmux switching

---
+++ lua/plugins/tmux.lua
@@ -0,0 +1,9 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  keys = {
+    { "<C-j>", "<cmd>TmuxNavigateDown<CR>" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<CR>" },
+    { "<C-h>", "<cmd>TmuxNavigateLeft<CR>" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<CR>" },
+  },
+}
