53960ee5fe0212b7dfdae716b7027a75933ff010
amilpa <amilpa2020@gmail.com>
2023-05-12 18:41:19 +0530

    setup neotree

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,7 +2,7 @@
 return {
   -- { "folke/which-key.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
   { "echasnovski/mini.animate", enabled = false },
