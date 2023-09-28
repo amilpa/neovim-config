f8cecd63deb055a8423b2dadefea169cd27dcb6f
Amil P A <amilpa2020@gmail.com>
2023-09-28 17:28:51 +0530

    enable noice

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,7 +3,7 @@ return {
   -- { "folke/which-key.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
+  -- { "folke/noice.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
   -- { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.animate", enabled = false },
