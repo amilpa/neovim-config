a6d0828ca6186dc3d3afaacb38db13c175f31f0b
Amil P A <amilpa2020@gmail.com>
2023-09-30 10:36:51 +0530

    add desc

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,8 +1,8 @@
 return {
   "tpope/vim-fugitive",
   keys = {
-    { "<leader>gg", vim.cmd.Git },
-    { "<leader>gd", "<cmd>Gvdiffsplit<CR>" },
-    { "<leader>gp", "<cmd>Git push<CR>" },
+    { "<leader>gg", vim.cmd.Git, desc = "Git status" },
+    { "<leader>gd", "<cmd>Gvdiffsplit<CR>", desc = "Vertical split diff" },
+    { "<leader>gp", "<cmd>Git push<CR>", desc = "Git push" },
   },
 }
