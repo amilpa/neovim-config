f6f2077ce14812f3ede92b855472a418ffe31b3a
Amil P A <amilpa2020@gmail.com>
2023-10-01 19:26:54 +0530

    add html to js

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -7,6 +7,7 @@ return {
     "rafamadriz/friendly-snippets",
     config = function()
       require("luasnip").filetype_extend("javascriptreact", { "html" })
+      require("luasnip").filetype_extend("javascript", { "html" })
       require("luasnip").filetype_extend("typescriptreact", { "html" })
       require("luasnip.loaders.from_vscode").lazy_load()
       require("luasnip.loaders.from_vscode").lazy_load({
