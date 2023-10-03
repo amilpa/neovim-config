01d5ea498da73b0fc10f7356f6af4a9e782dce0a
Amil P A <amilpa2020@gmail.com>
2023-10-03 20:26:02 +0530

    restart which-key

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 -- disable trouble
 return {
-  { "folke/which-key.nvim", enabled = false },
+  -- { "folke/which-key.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   -- { "folke/noice.nvim", enabled = false },
