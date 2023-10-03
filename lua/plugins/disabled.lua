550a049a219afa0397b8be786cf2bc5dcfd021ca
Amil P A <amilpa2020@gmail.com>
2023-10-03 19:30:17 +0530

    disable some

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,10 +1,12 @@
 -- disable trouble
 return {
-  -- { "folke/which-key.nvim", enabled = false },
+  { "folke/which-key.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
+  { "Shatur/neovim-session-manager", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   -- { "folke/noice.nvim", enabled = false },
-  { "folke/persistence.nvim", enabled = false },
+  -- { "folke/persistence.nvim", enabled = false },
   -- { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.animate", enabled = false },
   -- { "lewis6991/gitsigns.nvim", enabled = false },
