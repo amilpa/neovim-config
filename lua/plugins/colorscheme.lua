3aafe70a22d78f26bf8ad363a563d21b31865b1e
Amil P A <amilpa2020@gmail.com>
2023-10-14 19:51:29 +0530

    vscode

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,20 @@
-return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = { style = "night" },
-  config = function()
-    require("tokyonight").setup({
-      transparent = true,
-      on_highlights = function(hl, c)
-        local prompt = "#2d3149"
-        hl.TelescopeNormal = {
-          bg = "None",
-          fg = c.fg_dark,
-        }
-      end,
-    })
-  end,
-}
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = true,
+--   opts = { style = "night" },
+--   config = function()
+--     require("tokyonight").setup({
+--       transparent = true,
+--       on_highlights = function(hl, c)
+--         local prompt = "#2d3149"
+--         hl.TelescopeNormal = {
+--           bg = "None",
+--           fg = c.fg_dark,
+--         }
+--       end,
+--     })
+--   end,
+-- }
 -- return {
 --   "ellisonleao/gruvbox.nvim",
 --   lazy = true,
@@ -35,12 +35,12 @@ return {
 --     },
 --   },
 -- }
--- return {
---   "Mofiqul/vscode.nvim",
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "vscode",
---     },
---   },
--- }
+return {
+  "Mofiqul/vscode.nvim",
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+}
