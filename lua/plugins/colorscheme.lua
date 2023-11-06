d56439f0ff1a00b025272e674c852cbb48a4bb1f
amilpa <amilpa2020@gmail.com>
2023-11-06 22:21:45 +0530

    try stuff

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -16,22 +16,14 @@ return {
   end,
 }
 -- return {
---   "ellisonleao/gruvbox.nvim",
---   lazy = true,
--- }
--- return {
 --   {
---     "rose-pine/neovim",
---     config = function()
---       require("rose-pine").setup({
---         disable_italics = true,
---       })
---     end,
+--     "olivercederborg/poimandres.nvim",
+--     lazy = true,
 --   },
 --   {
 --     "LazyVim/LazyVim",
 --     opts = {
---       colorscheme = "rose-pine",
+--       colorscheme = "poimandres",
 --     },
 --   },
 -- }
