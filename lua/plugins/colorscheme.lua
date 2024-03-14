44a5287f963bea5ef49344cad997418a7e366b6c
amilpa <amilpa2020@gmail.com>
2024-03-14 17:46:51 +0530

    poimandres

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,32 +1,37 @@
-return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = { style = "night" },
-  config = function()
-    require("tokyonight").setup({
-      transparent = true,
-      on_highlights = function(hl, c)
-        local prompt = "#2d3149"
-        hl.TelescopeNormal = {
-          bg = "None",
-          fg = c.fg_dark,
-        }
-      end,
-    })
-  end,
-}
 -- return {
---   {
---     "olivercederborg/poimandres.nvim",
---     lazy = true,
---   },
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "poimandres",
---     },
---   },
+--   "folke/tokyonight.nvim",
+--   lazy = true,
+--   opts = { style = "night" },
+--   config = function()
+--     require("tokyonight").setup({
+--       transparent = true,
+--       on_highlights = function(hl, c)
+--         local prompt = "#2d3149"
+--         hl.TelescopeNormal = {
+--           bg = "None",
+--           fg = c.fg_dark,
+--         }
+--       end,
+--     })
+--   end,
 -- }
+return {
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = true,
+    config = function()
+      require("poimandres").setup({
+        disable_background = true,
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "poimandres",
+    },
+  },
+}
 -- return {
 --   "Mofiqul/vscode.nvim",
 --   {
