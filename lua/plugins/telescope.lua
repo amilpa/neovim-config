5c62d463b292383f666fdbb3ad4941b453922e14
amilpa <amilpa2020@gmail.com>
2024-03-14 21:31:21 +0530

    add resume

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,5 +2,6 @@ return {
   "nvim-telescope/telescope.nvim",
   keys = {
     { "<leader><space>", "<cmd>Telescope find_files<CR>", desc = "Find files" },
+    { "<leader>r", "<cmd>Telescope resume<CR>", desc = "Resume" },
   },
 }
