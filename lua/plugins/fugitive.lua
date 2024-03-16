a48f63fda415ab9290589559f308376a14d5934f
amilpa <amilpa2020@gmail.com>
2024-03-16 12:32:21 +0530

    remove fugitive

--- lua/plugins/fugitive.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "tpope/vim-fugitive",
-  event = "BufRead",
-  keys = {
-    { "<leader>gg", vim.cmd.Git, desc = "Git status" },
-    { "<leader>gd", "<cmd>Gvdiffsplit<CR>", desc = "Vertical split diff" },
-    { "<leader>gp", "<cmd>Git push<CR>", desc = "Git push" },
-  },
-}
