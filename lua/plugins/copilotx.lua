febe6889225e509a12199f20ff988d803f41db7a
amilpa <amilpa2020@gmail.com>
2024-03-16 16:29:31 +0530

    add event

--- lua/plugins/copilotx.lua
+++ lua/plugins/copilotx.lua
@@ -1,6 +1,7 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
+    event = "BufReadPre",
     branch = "canary",
     dependencies = {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
