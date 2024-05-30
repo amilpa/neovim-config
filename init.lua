4a532762c9670a4ac8eb25d77f10f718a9e159b6
amilpa <amilpa2020@gmail.com>
2024-05-30 11:10:47 +0530

    remap telescope

--- init.lua
+++ init.lua
@@ -353,11 +353,11 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<C-f>'] = 'select_vertical', ['<C-h>'] = 'select_horizontal' },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
