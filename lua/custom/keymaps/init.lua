fcc52a706cf41362e6b4b434498caca3bdb64899
amilpa <amilpa2020@gmail.com>
2024-05-30 10:56:03 +0530

    add select all keymap

--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -2,4 +2,5 @@ vim.keymap.set('n', '<leader>pv', '<cmd>Oil<CR>', { desc = 'Go to file explorer'
 vim.keymap.set('n', '<C-s>', '<cmd>w<CR>', { desc = 'Save file' })
 vim.keymap.set('n', '<S-l>', '<cmd>b#<CR>', { desc = 'Switch to recent buffer' })
 vim.keymap.set('n', '<leader>qq', '<cmd>q<CR>', { desc = 'Quit' })
+vim.keymap.set('n', '<leader>a', 'gg<S-v>G')
 return {}
